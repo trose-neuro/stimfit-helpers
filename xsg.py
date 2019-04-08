6c893944e00ca92df072193077d388bf615688b7
Tobias Rose <trose@neuro.mpg.de>
2019-04-08 10:18:39 +0200

    python loader mod

--- xsg.py
+++ xsg.py
@@ -1,4 +1,5 @@
 import stf
+import stfio
 import wx as wx
 import scipy.io as sio
 import numpy as np
@@ -23,46 +24,48 @@ def load():
     stf.set_xunits('ms')
     stf.set_yunits('pA')
 
-def get_amplitude(trace=None):
-    """ Calculates the amplitude deviation (peak-base) in units of the Y-axis
-
-    Arguments:
-    base        -- Starting point (in ms) of the baseline cursor.
-    peak        -- Starting point (in ms) of the peak cursor.
-    delta       -- Time interval to calculate baseline/find the peak.
-    trace       -- Zero-based index of the trace to be processed, if None then current
-                    trace is computed.
-
-
-    Returns:
-    A float with the variation of the amplitude. False if
-
-    Example:
-    get_amplitude(980,1005,10,i) returns the variation of the Y unit of the trace i between
-    peak value (10050+10) msec and baseline (980+10) msec
-    """
-
-    # sets the current trace or the one given in trace
-    if trace is None:
-        sweep = stf.get_trace_index()
-    else:
-        if typ(trace) != int:
-            print "trace argument admits only intergers"
-            return False
-        sweep = trace
-
-
-    # # set base cursors:
-    # if not(stf.set_base_start(base,True)): return False # out-of range
-    # if not(stf.set_base_end(base+delta,True)): return False
-    #
-    # # set peak cursors:
-    # if not(stf.set_peak_start(peak,True)): return False # out-of range
-    # if not(stf.set_peak_end(peak+delta,True)): return False
-
-    # update measurements
-    stf.set_trace(sweep)
-
-    amplitude = stf.get_peak()-stf.get_base()
-
-    return amplitude
+# def get_amplitude(trace=None):
+#     """ Calculates the amplitude deviation (peak-base) in units of the Y-axis
+#
+#     Arguments:
+#     base        -- Starting point (in ms) of the baseline cursor.
+#     peak        -- Starting point (in ms) of the peak cursor.
+#     delta       -- Time interval to calculate baseline/find the peak.
+#     trace       -- Zero-based index of the trace to be processed, if None then current
+#                     trace is computed.
+#
+#
+#     Returns:
+#     A float with the variation of the amplitude. False if
+#
+#     Example:
+#     get_amplitude(980,1005,10,i) returns the variation of the Y unit of the trace i between
+#     peak value (10050+10) msec and baseline (980+10) msec
+#     """
+#
+#     # sets the current trace or the one given in trace
+#     if trace is None:
+#         sweep = stf.get_trace_index()
+#     else:
+#         if typ(trace) != int:
+#             print "trace argument admits only intergers"
+#             return False
+#         sweep = trace
+#
+#
+#     # # set base cursors:
+#     # if not(stf.set_base_start(base,True)): return False # out-of range
+#     # if not(stf.set_base_end(base+delta,True)): return False
+#     #
+#     # # set peak cursors:
+#     # if not(stf.set_peak_start(peak,True)): return False # out-of range
+#     # if not(stf.set_peak_end(peak+delta,True)): return False
+#
+#     # update measurements
+#     stf.set_trace(sweep)
+#
+#     amplitude = stf.get_peak()-stf.get_base()
+#
+#     return amplitude
+# def save():
+#     stf.file_save("/Users/trose/Data/test.dat")
